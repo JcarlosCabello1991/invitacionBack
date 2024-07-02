7d222a019522d89c34da94df9e556f620e1e7df6
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2024-07-02 14:41:50 +0200

    m

--- index.js
+++ index.js
@@ -41,9 +41,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use('/invitados',invitados);
 
 // Start Server
-const server = app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-server.keepAliveTimeout = 120 * 1000; // 120 segundos
-server.headersTimeout = 120 * 1000; // 120 segundos
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
